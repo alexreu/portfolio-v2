3334fedfeebe44f7700479b18a0b9ea4a7092a98
Alexandre ADOLPHE <aadolphe@simplonprod.co>
2022-03-13 15:46:40 +0400

    - Add landscape fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,13 @@ import styles from '../../styles/Index.module.css'
 
 const Index = () => {
     return (
-        <section className="flex items-center justify-center mx-auto ">
-            <div className="container flex flex-col items-center mx-auto text-center">
-                <h2 className="lgd:mb-4 font-medium text-[36px] sm:text-[46px] md:text-[64px] lg:text-[100px] xl:text-[120px] 2xl:text-[140px] lg:tracking-widest text-white uppercase">Coming
-                    Soon !</h2>
-                <p className="order-first text-[18px] md:text-[24px] lg:text-[48px] text-white opacity-70 uppercase">site under reconstruction</p>
-                <a href="mailto:contact@alexandreadolphe.com" aria-label="contact alexandre adolphe" className={styles.contactLink}>Contact</a>
-            </div>
+        <section className="container flex flex-col items-center justify-center mx-auto text-center landscape:lgd:mt-4">
+            <h2 className="lgd:mb-4 font-medium text-[36px] sm:text-[46px] md:text-[64px] lg:text-[100px] xl:text-[120px] 2xl:text-[140px] lg:tracking-widest text-white uppercase">Coming
+                Soon !</h2>
+            <p className="order-first text-[18px] md:text-[24px] lg:text-[48px] text-white opacity-70 uppercase">site
+                under reconstruction</p>
+            <a href="mailto:contact@alexandreadolphe.com" aria-label="contact alexandre adolphe"
+               className={styles.contactLink}>Contact</a>
         </section>
     )
 }
