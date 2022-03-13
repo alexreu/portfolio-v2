4d6aac8fcd04c9818c2098c02028308ed2bfdea3
Alexandre ADOLPHE <aadolphe@simplonprod.co>
2022-03-13 16:03:03 +0400

    - Fix performance issue

--- src/components/layouts/layout.js
+++ src/components/layouts/layout.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 import Footer from "./footer";
 import Navbar from "./navbar";
 
@@ -10,8 +11,16 @@ const Layout = ({children}) => {
                 <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
                 <meta name="description" content="Alexandre ADOLPHE - Portfolio"/>
             </Head>
-            <div className="flex flex-col relative z-0 h-screen w-screen overflow-hidden p-4 bg-[url('/images/bg-under-construction.jpeg')]
-            bg-no-repeat bg-cover bg-center before:absolute before:h-full before:w-full before:top-0 before:left-0 before:-z-[1] before:bg-black before:bg-opacity-50">
+            <div className="flex flex-col relative z-0 h-screen w-screen overflow-hidden p-4 before:absolute before:h-full before:w-full before:top-0 before:left-0 before:-z-[1] before:bg-black before:bg-opacity-50">
+                <Image
+                    src="/images/bg-under-construction.jpeg"
+                    className="-z-[2]"
+                    alt=""
+                    layout="fill"
+                    objectFit="cover"
+                    objectPosition="center"
+                    loading="eager"
+                />
                 <Navbar />
                 <main className="mt-auto font-main">
                     {children}
