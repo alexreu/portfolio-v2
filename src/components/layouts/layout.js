fa818a2af673ddcec4ac6b90bcede9bdeb098ef9
Alexandre ADOLPHE <aadolphe@simplonprod.co>
2022-03-14 08:45:00 +0400

    - Fix background image

--- src/components/layouts/layout.js
+++ src/components/layouts/layout.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Footer from "./footer";
 import Navbar from "./navbar";
+import backgroundPic from '../../../public/images/bg-under-construction.jpeg'
 
 const Layout = ({children}) => {
     return (
@@ -12,17 +13,17 @@ const Layout = ({children}) => {
                 <meta name="description" content="Alexandre ADOLPHE - Portfolio"/>
             </Head>
             <div className="flex flex-col relative z-0 h-screen w-screen overflow-hidden p-4 before:absolute before:h-full before:w-full before:top-0 before:left-0 before:-z-[1] before:bg-black before:bg-opacity-50">
+                <Image
+                    src={backgroundPic}
+                    className="-z-[2]"
+                    alt=""
+                    layout="fill"
+                    objectFit="cover"
+                    objectPosition="center"
+                    loading="eager"
+                />
                 <Navbar />
                 <main className="mt-auto font-main">
-                    <Image
-                        src="/images/bg-under-construction.jpeg"
-                        className="-z-[2]"
-                        alt=""
-                        layout="fill"
-                        objectFit="cover"
-                        objectPosition="center"
-                        loading="eager"
-                    />
                     {children}
                 </main>
                 <Footer />
