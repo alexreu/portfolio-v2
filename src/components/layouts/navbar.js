d0c85cb4d34e0c01482d5331ae7c0c28a53e4dfc
Alexandre ADOLPHE <aadolphe@simplonprod.co>
2022-04-03 12:11:06 +0400

    WIP - navarb

--- src/components/layouts/navbar.js
+++ src/components/layouts/navbar.js
@@ -2,26 +2,8 @@ import styles from '../../../styles/Navbar.module.css';
 import {useEffect, useState} from "react";
 
 const Navbar = () => {
-    const navContent = [
-        {label: 'Accueil', icon: 'house-door-fill', anchor:'home'},
-        {label: 'A propos', icon: 'question-circle-fill', anchor:'about'},
-        {label: 'Diplômes', icon: 'mortarboard-fill', anchor:'graduations'},
-        {label: 'Portfolio', icon: 'award-fill', anchor:'portfolio'},
-        {label: 'Contact', icon: 'send-fill', anchor:'contact'},
-    ]
-    const navList = navContent.map((navItem, index) => {
-        const {anchor, label, icon} = navItem
-        return (
-            <li key={index} className="font-bold transition-200 hover:text-primary">
-                <a href={`#${anchor}`} className="custom-underline inline-flex items-center h-full gap-x-4">
-                    <span>{label}</span>
-                    <i className={`bi bi-${icon}`}/>
-                </a>
-            </li>
-            )
-    });
-
     const [scroll, setScroll] = useState(false);
+    const [openMenu, setOpenMenu] = useState(false);
 
     useEffect(() => {
         const onScroll = () => {
@@ -34,10 +16,38 @@ const Navbar = () => {
         return () => document.removeEventListener("scroll", onScroll)
     }, [scroll])
 
+    const handleToggleMenu = () => {
+        setOpenMenu(!openMenu);
+    }
+    const navContent = [
+        {label: 'Accueil', icon: 'house-door-fill', anchor:'home'},
+        {label: 'A propos', icon: 'question-circle-fill', anchor:'about'},
+        {label: 'Diplômes', icon: 'mortarboard-fill', anchor:'graduations'},
+        {label: 'Portfolio', icon: 'award-fill', anchor:'portfolio'},
+        {label: 'Contact', icon: 'send-fill', anchor:'contact'},
+    ]
+    const navList = navContent.map((navItem, index) => {
+        const {anchor, label, icon} = navItem
+        return (
+            <li key={index} className="lgd:w-full transition-200 hover:text-primary">
+                <a href={`#${anchor}`} className="custom-underline inline-flex items-center h-full gap-x-4">
+                    <span>{label}</span>
+                    <i className={`bi bi-${icon}`}/>
+                </a>
+            </li>
+            )
+    });
+
+
+
     return (
-        <header role="banner" className={(scroll ? `${styles.headerSticky} transform fixed top-0 bg-grey-dark text-white transition-all duration-500 ease-in-out` : 'absolute top-0 text-black bg-transparent transition-200') + ' w-full' } >
-            <nav role="navigation" aria-label="navigation-principal">
-                <ul className="flex justify-center items-stretch gap-x-16 min-h-[80px] text-[22px]">
+        <header role="banner" className={(scroll ? `${styles.headerSticky} fixed top-0 z-[9999] bg-white` : 'absolute top-0 bg-transparent') + ` w-full transition-all duration-300 ease-in-out` } >
+            <button className='lg:hidden' onClick={handleToggleMenu}>Open Menu</button>
+            <nav role="navigation" aria-label="navigation-principal" className={(openMenu ? 'lgd:w-screen lgd:translate-x-0': 'lgd:w-0 lgd:-translate-x-full') + ` ${styles.mobilePopUp}`}>
+                <ul className={"lg:flex justify-center items-stretch gap-x-16 lg:min-h-[80px] text-[22px] transition-200"}>
+                    <li>
+                        <button className='lg:hidden' onClick={handleToggleMenu}>Close Menu</button>
+                    </li>
                     {navList}
                 </ul>
             </nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,14 @@ import backgroundImage from '../../public/images/bg-under-construction.jpeg';
 const Index = () => {
     return (
         <section className="bg-test h-[1800px]">
+            <span>t</span>
             {/*<Image src={backgroundImage} layout='fill' objectFit='cover' objectPosition='center' style='-z-[9999] bg-fixed'/>*/}
-            <h1 className="lgd:mb-4 font-medium text-[36px] sm:text-[46px] md:text-[64px] lg:text-[100px] xl:text-[120px] 2xl:text-[140px] lg:tracking-widest uppercase">Coming
-                Soon !</h1>
-            <p className="order-first text-[18px] md:text-[24px] lg:text-[48px] opacity-70 uppercase">site
-                under reconstruction</p>
-            <a href="mailto:contact@alexandreadolphe.com" aria-label="contact alexandre adolphe"
-               className={styles.contactLink}>Contact</a>
+            {/*<h1 className="lgd:mb-4 font-medium text-[36px] sm:text-[46px] md:text-[64px] lg:text-[100px] xl:text-[120px] 2xl:text-[140px] lg:tracking-widest uppercase">Coming*/}
+            {/*    Soon !</h1>*/}
+            {/*<p className="order-first text-[18px] md:text-[24px] lg:text-[48px] opacity-70 uppercase">site*/}
+            {/*    under reconstruction</p>*/}
+            {/*<a href="mailto:contact@alexandreadolphe.com" aria-label="contact alexandre adolphe"*/}
+            {/*   className={styles.contactLink}>Contact</a>*/}
         </section>
     )
 }
