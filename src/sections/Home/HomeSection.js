dff1132bbccb077f34d4d0ea84973954542df6c3
Alexandre ADOLPHE <aadolphe@simplonprod.co>
2022-08-03 20:40:05 +0400

    WIP - home section

--- src/sections/Home/HomeSection.js
+++ src/sections/Home/HomeSection.js
@@ -6,13 +6,13 @@ const HomeSection = () => {
     return (
         <section className={styles.homeBackground}>
             <div className="container relative z-10">
+                <span className="inline-flex w-full mb-5 font-sub text-grey-light text-base uppercase opacity-70 tracking-[4px]">Developpeur Front end</span>
                 <h1 className="flex items-center flex-col text-white text-[58px] leading-[75px] font-montserrat font-black">
                     <span className="w-full">Alexandre Adolphe</span>
                     <span className="w-full text-primary">Developpeur JS.</span>
                 </h1>
-                <p className="mt-2.5 mb-7 text-[54px] text-white font-main leading-none">Situé à l'île de la Réunion</p>
-                <span className="inline-flex w-full">Front end developpeur</span>
-                <Button type="default" style="text-white hover:text-primary">Voir mon cv</Button>
+                <p className="mt-2.5 mb-7 text-[54px] text-white font-main leading-none">Situé à l'île de la Réunion.</p>
+                <Button type="default" style="text-white border-grey-light">Voir mon cv</Button>
             </div>
         </section>
     );
