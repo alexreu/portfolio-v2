670cb4b3441fbd14fbccaf285c5a62a8b12d07f2
Alexandre ADOLPHE <aadolphe@simplonprod.co>
2022-08-08 20:24:29 +0400

    - WIP services section

--- src/components/Sections/Services/ServicesSection.js
+++ src/components/Sections/Services/ServicesSection.js
@@ -6,8 +6,16 @@ import { SectionSubtitle } from "../../SectionSubtitle";
 export const ServicesSection = () => {
     return (
         <SectionWrapper>
-            <SectionSubtitle>Que puis-je faire pour vous ?</SectionSubtitle>
-            <Heading variant="h2">Mes incroyables services</Heading>
+            <div className="container text-center">
+                <SectionSubtitle>Que puis-je faire pour vous ?</SectionSubtitle>
+                <Heading variant="h2" className="mb-5 text-[32px] lg:text-[50px] text-heading font-main font-semibold">
+                    Mes incroyables services
+                </Heading>
+                <p className="px-[25%] text-lg text-gray">
+                    There are many variations of passages of Lorem Ipsum available, but the majority have suffered
+                    alteration.
+                </p>
+            </div>
         </SectionWrapper>
     );
 };
