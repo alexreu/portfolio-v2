1f62b0d151f67d040a1244d44d994403be72e0c3
Alexandre ADOLPHE <aadolphe@simplonprod.co>
2022-08-16 15:41:20 +0400

    - Refacto data fetching with getStaticProps

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { AboutSection, HomeSection, PortfolioSection, ServicesSection } from "../components/Sections";
-import { fetcher } from "../modules/fetcher";
-import { Loader } from "../components/Loader";
-import { RessourceType } from "../enums";
+import { getDirectoryPath } from "../modules/getDirectoryPath/getDirectoryPath";
+import ressourceType from "../enums/RessourceType";
+import { getJsonContent } from "../modules/getJsonContent/getJsonContent";
 
-const Index = () => {
-    const { ABOUT, SERVICES, PROJECTS } = RessourceType;
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(false);
-    const [datas, setDatas] = useState(null);
-
-    const getAllData = async () => {
-        return await Promise.all([fetcher(`/api/${ABOUT}`), fetcher(`/api/${SERVICES}`), fetcher(`/api/${PROJECTS}`)]);
-    };
-
-    useEffect(() => {
-        setIsLoading(true);
-        try {
-            getAllData().then(res => {
-                res.forEach(data => {
-                    const [currentKey] = Object.keys(data);
-                    setDatas(prevState => ({ ...prevState, [currentKey]: data[currentKey] }));
-                });
-                setIsLoading(false);
-            });
-        } catch (e) {
-            setIsLoading(false);
-            setIsError(true);
-            console.log(e);
-        }
-    }, []);
-
-    if (isLoading) return <Loader />;
-
+const Index = ({ about, services, projects }) => {
     return (
         <>
             <HomeSection />
-            <AboutSection data={datas.about} />
-            <ServicesSection services={datas.services} />
-            <PortfolioSection projects={datas.projects} />
+            <AboutSection data={about} />
+            <ServicesSection services={services} />
+            <PortfolioSection projects={projects} />
         </>
     );
 };
+
+export async function getStaticProps() {
+    const { ABOUT, PROJECTS, SERVICES } = ressourceType;
+    const jsonDirectory = getDirectoryPath("json");
+    const aboutData = await getJsonContent(jsonDirectory, ABOUT);
+    const servicesData = await getJsonContent(jsonDirectory, SERVICES);
+    const projectsData = await getJsonContent(jsonDirectory, PROJECTS);
+    return {
+        props: { ...aboutData, ...servicesData, ...projectsData },
+    };
+}
 
 export default Index;
