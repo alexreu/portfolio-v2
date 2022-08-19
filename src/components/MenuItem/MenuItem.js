06465480aa6c6f54b72c1c5c57b122bd07dcf97e
Alexandre ADOLPHE <aadolphe@simplonprod.co>
2022-08-19 19:27:19 +0400

    - Fix navlink bug color

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -6,7 +6,7 @@ export const MenuItem = ({ anchor, label, scroll, isUnderline, onClick }) => {
             key={anchor}
             id={`menuItem${anchor.charAt(0).toUpperCase() + anchor.slice(1)}`}
             className={`${
-                scroll ? "text-black dark:lg:text-white" : "text-black dark:text-white lg:text-white"
+                scroll ? "text-black dark:text-white" : "text-black dark:text-white lg:text-white"
             } lgd:py-2 lgd:w-full transition-200 duration-300 dark:hover:text-primary hover:text-primary lgd:border-b border-grey-light`}
         >
             <a
