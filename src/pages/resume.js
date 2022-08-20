1afa268243f18eb7f7691ece49ccfae89c52f2e5
Alexandre ADOLPHE <aadolphe@simplonprod.co>
2022-08-20 18:01:00 +0400

    - Restrict access of resume with smartphone and tablet

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ResumeWrapper } from "../components/Resume";
 import ressourceType from "../enums/RessourceType";
 import { getDirectoryPath } from "../modules/getDirectoryPath/getDirectoryPath";
 import { getJsonContent } from "../modules/getJsonContent/getJsonContent";
 import Layout from "../components/Layout/Layout";
+import { Loader } from "../components/Loader";
 
-const Resume = ({ about }) => <ResumeWrapper data={about} />;
+const Resume = ({ about }) => {
+    const [showResume, setShowResume] = useState(false);
+    useEffect(() => {
+        const checkWindowWidth = () => {
+            if (window.innerWidth <= 1170) {
+                setShowResume(false);
+            } else {
+                setShowResume(true);
+            }
+        };
+        checkWindowWidth();
+        window.addEventListener("resize", checkWindowWidth);
+    }, []);
+
+    return showResume ? <ResumeWrapper data={about} /> : <Loader />;
+};
 
 Resume.getLayout = page => (
     <Layout showNavbar={false} showFooter={false} title="Curriculum vitae">
