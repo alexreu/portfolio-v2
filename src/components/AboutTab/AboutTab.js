b5eabcb6716e445a1fe0227c04bb80b8abd0a60b
Alexandre ADOLPHE <aadolphe@simplonprod.co>
2022-09-26 20:26:57 +0400

    - Remove about css - Remove unfold button - Add about item component - Update tab change animation

--- src/components/AboutTab/AboutTab.js
+++ src/components/AboutTab/AboutTab.js
@@ -1,34 +1,12 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
-import styles from "./AboutTab.module.css";
 import { AboutTabItem } from "../AboutTabItem";
 
 export const AboutTab = ({ navTabsContent }) => {
-    const aboutTabContent = useRef(null);
     const [currentTab, setCurrentTab] = useState("skills");
-    const [tabContent, setTabContent] = useState(navTabsContent.filter(e => e.id === currentTab));
-    const [displayShowMoreButton, setDisplayShowMoreButton] = useState(false);
-    const [tabContentHeight, setTabContentHeight] = useState(210);
-    const [tabFold, setTabFold] = useState(true);
 
     const handleTabClick = tabId => {
         setCurrentTab(tabId);
-        if (tabId !== currentTab) {
-            setTabContent(navTabsContent.filter(e => e.id === tabId));
-        }
-    };
-
-    const handleMoreButtonClick = () => {
-        setTabFold(!tabFold);
-    };
-    const tabContentVariants = {
-        fold: { maxHeight: 210 },
-        unFold: { maxHeight: 400 },
-    };
-
-    const tabControlVariants = {
-        selected: { backgroundColor: "#FD4766", color: "#FFFFFF" },
-        unSelected: { color: "rgba(29,29,36,.75)" },
     };
 
     const spring = {
@@ -37,40 +15,38 @@ export const AboutTab = ({ navTabsContent }) => {
         damping: 30,
     };
 
-    useEffect(() => {
-        // const aboutTabContentHeight = aboutTabContent.current.getClientRects()[0].height;
-        // if (tabContentHeight !== aboutTabContentHeight || tabContentHeight === 210) {
-        //     setDisplayShowMoreButton(aboutTabContentHeight >= 210);
-        //     setTabContentHeight(aboutTabContentHeight);
-        // }
-    }, [tabContent, tabContentHeight]);
-
     return (
         <div className="mt-7">
-            <ul className={styles.tabList}>
-                {navTabsContent.map(tab => (
-                    <motion.li key={tab.title} className={`relative text-sm ${currentTab === tab.id && "text-white"}`}>
-                        <button
-                            type="button"
-                            aria-controls="aboutTabContent"
-                            onClick={() => handleTabClick(tab.id)}
-                            key={tab.id}
-                            id={tab.id}
-                            className="relative z-[2] px-[13.5px] -my-px -mx-0.5 lg:px-6 py-3 lg:py-3 hover:cursor-pointer font-semibold dark:!text-white"
-                        >
-                            {tab.title}
-                        </button>
-                        {currentTab === tab.id ? (
-                            <motion.div
-                                layoutId="underline"
-                                transition={spring}
-                                className="absolute z-[1] w-full h-full top-0 left-0 right-0 rounded-full bg-primary"
-                            />
-                        ) : null}
-                    </motion.li>
-                ))}
+            <ul
+                className="flex items-center md:justify-between mdd:flex-wrap mdd:pb-2 font-main text-sm font-semibold
+            md:border mdd:border-b border-border dark:border-border md:rounded-full"
+            >
+                {navTabsContent.map(tab => {
+                    const { id, title } = tab;
+                    return (
+                        <motion.li key={id} className={`relative text-sm ${currentTab === id && "text-white"}`}>
+                            <button
+                                type="button"
+                                aria-controls={id}
+                                onClick={() => handleTabClick(id)}
+                                key={id}
+                                id={id}
+                                className="relative z-[2] px-[13.5px] -my-px -mx-0.5 lg:px-6 py-3 lg:py-3 hover:cursor-pointer font-semibold dark:!text-white"
+                            >
+                                {title}
+                            </button>
+                            {currentTab === id ? (
+                                <motion.div
+                                    layoutId="background"
+                                    transition={spring}
+                                    className="absolute z-[1] w-full h-full top-0 left-0 right-0 rounded-full bg-primary"
+                                />
+                            ) : null}
+                        </motion.li>
+                    );
+                })}
             </ul>
-            <div id="aboutTabContent">
+            <div id={currentTab}>
                 <AboutTabItem item={navTabsContent.filter(e => e.id === currentTab)} />
             </div>
         </div>
--- src/components/Sections/About/AboutSection.js
+++ src/components/Sections/About/AboutSection.js
@@ -12,7 +12,7 @@ export const AboutSection = ({ data }) => {
                 <div className="col-span-5 relative rounded-lg overflow-hidden min-h-[300px] lg:max-h-[691px]">
                     <Image
                         src={aboutMe}
-                        className={"h-full object-cover object-left rounded-lg overflow-hidden"}
+                        className={"h-full object-cover object-left lgd:mx-auto rounded-lg overflow-hidden"}
                         alt=""
                         height={691}
                         width={446}
