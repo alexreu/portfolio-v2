c1fd198c32192d01624ef4114ebb051afb302fd3
Alexandre Adolphe <alexandre.adolphepro@gmail.com>
2024-02-21 15:10:04 +0400

    WIP - integrate sanity studio

---
+++ studio/schemaTypes/homepage.ts
@@ -0,0 +1,27 @@
+export default {
+    name: "homepage",
+    title: "Contenu de la page d'accueil",
+    type: "document",
+    fields: [
+        {
+            name: "job",
+            type: "string",
+            title: "Poste"
+        },
+        {
+            name: "years_of_experiences",
+            type: "number",
+            title: "Années d'expériences"
+        },
+        {
+            name: "number_of_projects",
+            type: "number",
+            title: "Nombre de projets réalisé"
+        },
+        {
+            name: "number_of_happy_customers",
+            type: "number",
+            title: "Nombre de clients satisfait"
+        }
+    ]
+}
