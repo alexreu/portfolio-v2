d1fcab014822ab21c9c14581db7e5eebcd9154d3
Alexandre Adolphe <alexandre.adolphepro@gmail.com>
2024-02-24 17:01:09 +0400

    Add missing file Update .env.example file

--- lib/sanity/client.ts
+++ lib/sanity/client.ts
@@ -1,12 +1,11 @@
-import { createClient, type ClientConfig } from "next-sanity";
+import { type ClientConfig, createClient } from "next-sanity";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-05-03";
 
 const config: ClientConfig = {
     projectId,
-    dataset,
+    dataset: "production",
     apiVersion,
     useCdn: false,
 };
