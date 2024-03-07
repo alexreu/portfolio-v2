1820167566c0262e96ac1798111271c03485b70c
Alexandre Adolphe <alexandre.adolphepro@gmail.com>
2024-03-07 18:43:11 +0400

    WIP - about data structure

---
+++ schemaTypes/about.ts
@@ -0,0 +1,72 @@
+import { defineArrayMember, defineField, defineType } from "@sanity/types";
+
+const about = defineType({
+    name: "About",
+    title: "Page à propos",
+    type: "document",
+    fields: [
+        defineField({
+            name: "presentation",
+            title: "Présentation",
+            type: "array",
+            of: [
+                defineArrayMember({
+                    type: "object",
+                    fields: [
+                        {
+                            name: "title",
+                            title: "Titre",
+                            type: "text",
+                            validation: rule => rule.required(),
+                        },
+                        {
+                            name: "description",
+                            title: "Description",
+                            type: "block",
+                            validation: rule => rule.required(),
+                        },
+                    ],
+                }),
+            ],
+        }),
+        defineField({
+            name: "experiences",
+            title: "Experiences",
+            type: "array",
+            of: [
+                defineArrayMember({
+                    type: "object",
+                    fields: [
+                        {
+                            name: "isCurrentJob",
+                            title: "Poste en cours ?",
+                            type: "boolean",
+                            options: {
+                                default: false,
+                            },
+                        },
+                        {
+                            name: "startYear",
+                            title: "Année de début",
+                            type: "date",
+                            options: {
+                                format: "YYYY",
+                            },
+                            validation: rule => rule.required(),
+                        },
+                        {
+                            name: "endYear",
+                            title: "Année de fin",
+                            type: "date",
+                            options: {
+                                format: "YYYY",
+                            },
+                        },
+                    ],
+                }),
+            ],
+        }),
+    ],
+});
+
+export default about;
