f6b34b233e26b2b297cc0823abdf2d007c71e872
Alexandre Adolphe <alexandre.adolphepro@gmail.com>
2024-03-14 18:39:35 +0400

    WIP - Test for color bug on mobile device

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
-    darkMode: ["class"],
+    darkMode: "selector",
     content: ["./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}"],
     theme: {
         container: {
