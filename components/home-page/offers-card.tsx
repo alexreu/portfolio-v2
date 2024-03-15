617c19de49add7d36106716667b3424aa09f0b64
Alexandre Adolphe <alexandre.adolphepro@gmail.com>
2024-03-15 09:50:52 +0400

    - Fix responsive issue

--- components/home-page/offers-card.tsx
+++ components/home-page/offers-card.tsx
@@ -6,17 +6,17 @@ import { CardFooterLink } from "@/components/ui/card-footer-link";
 export const OffersCard = () => {
     return (
         <Card
-            className="relarive order-8 col-span-1 flex flex-col opacity-100 backdrop-blur-xl lg:order-none
-                lg:col-span-3 lg:row-span-1 lg:justify-center"
+            className="relarive order-8 col-span-1 flex flex-col justify-center opacity-100 backdrop-blur-xl
+                lg:order-none lg:col-span-3 lg:row-span-1 lg:justify-end xl:justify-center"
         >
-            <CardHeader>
+            <CardHeader className="p-6 lg:p-3 xl:p-6">
                 <h1
-                    className="inline-flex flex-col gap-5 text-xl font-semibold leading-none tracking-wide text-white
-                        md:text-2xl lg:text-xl xl:text-2xl"
+                    className="inline-flex flex-col gap-5 text-2xl font-semibold leading-none tracking-wide text-white
+                        lg:text-xl xl:text-2xl"
                 >
                     <span>Votre site vitrine</span>
                     <span className="ml-12 lg:ml-0 xl:ml-5">
-                        à partir de <span className="text-primary">699 €</span>
+                        à partir de <span className="text-primary">1300 €</span>
                     </span>
                 </h1>
             </CardHeader>
